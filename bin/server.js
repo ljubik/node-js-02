af587e53c1d4a48b91a0256190265b1863988904
Liubomyr <ljubik@ukr.net>
2021-06-25 14:51:05 +0300

    prepare to hw 05

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,16 @@
-const app = require('../app.js')
-const db = require('../model/db.js')
+const app = require('../app')
+const db = require('../model/db')
+const createFolderIsNotExist = require('../helpers/create-folder')
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
+const UPLOAD_DIR = process.env.UPLOAD_DIR
+const AVATAR_OF_USERS = process.env.AVATAR_OF_USERS
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    await createFolderIsNotExist(UPLOAD_DIR)
+    await createFolderIsNotExist(AVATAR_OF_USERS)
     console.log(`Server running. Use our API on port: ${PORT}`)
   })
 }).catch((e) => {
